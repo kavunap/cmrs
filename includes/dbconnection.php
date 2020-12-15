edf787d68abc334353d14ff99cad1f476614e54f
kavunap <pkavuna@gmail.com>
2020-12-15 14:29:19 +0100

    composer file

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,6 +1,10 @@
 <?php
+//development connection
+
 //$con=mysqli_connect("localhost", "root", "", "crmsdb");
-$con=mysqli_connect("us-cdbr-east-02.cleardb.com", "b879d7a55bdd88", "199c32a4", "heroku_7f476346699d35d");
+
+//remote database connection
+$con=mysqli_connect("remotemysql.com", "50T8ZnQNNJ", "hXJ7vXn34M", "50T8ZnQNNJ");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
