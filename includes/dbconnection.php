4e42f5285f5712f901fc4cc60788f94598493ffe
kavunap <pkavuna@gmail.com>
2020-12-15 15:37:36 +0100

    composer file

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,19 +1,19 @@
 <?php
 //development connection
 
-//$con=mysqli_connect("localhost", "root", "", "crmsdb");
+// $con=mysqli_connect("localhost", "root", "", "crmsdb");
 
-//remote database connection
-// $con=mysqli_connect("remotemysql.com", "50T8ZnQNNJ", "hXJ7vXn34M", "50T8ZnQNNJ");
+// //remote database connection
+// // $con=mysqli_connect("remotemysql.com", "50T8ZnQNNJ", "hXJ7vXn34M", "50T8ZnQNNJ");
 // if(mysqli_connect_errno()){
 // echo "Connection Fail".mysqli_connect_error();
 // }
 
-//remote database connection
-  $host = '';
-  $db = '';
-  $user = '';
-  $pass = '';
+// //remote database connection
+  $host = 'remotemysql.com';
+  $db = '50T8ZnQNNJ';
+  $user = '50T8ZnQNNJ';
+  $pass = 'hXJ7vXn34M';
   $charset = 'utf8mb4';
   $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
   try {
