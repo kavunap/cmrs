16af982d033925f37207f48d6673f5450cc46d8a
kavunap <pkavuna@gmail.com>
2020-12-15 13:16:09 +0100

    composer file

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "crmsdb");
+//$con=mysqli_connect("localhost", "root", "", "crmsdb");
+$con=mysqli_connect("us-cdbr-east-02.cleardb.com", "b879d7a55bdd88", "199c32a4", "heroku_7f476346699d35d");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
